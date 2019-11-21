07e63af618395890e0e0373bbbe5f5b05e0d74a4
Sunil Pai <threepointone@gmail.com>
2019-11-21 18:52:43 +0000

    fixes

--- src/index.test.js
+++ src/index.test.js
@@ -2,19 +2,40 @@ import React from "react";
 import App from "./App";
 import { create, act } from "react-test-renderer";
 
-let renderer2;
+// use fake timers when dealing with timeouts 
+jest.useFakeTimers();
 
 it("Should render App correctly without act()?", () => {
-  const renderer1 = create(<App />);
-  expect(true).toBeTruthy();
+  // this isn't a great test, especially because there's async logic involved. 
+  // it'll leak into other tests, unpredictably so 
+  // you might still get an act warning with this.
+  const tree = create(<App />);
+  expect(tree).toMatchInlineSnapshot(`
+    <div>
+      false
+    </div>
+  `);  
 });
 
-it("Should render App correctly with act()?", async () => {
+it("Should render App correctly with act()?", async () => {  
+  let tree;
   act(() => {
-    renderer2 = create(<App />);
+    tree = create(<App />);
   });
+  
+  // at this point, React would have 'committed',
+  // and effects would have been triggered (starting the promise in doSomething())
+
+  // act doesn't wait for timers or non-resolved promises,
+  // so let's use this snippet to 'wait' for them to resolve
   await act(async () => {
     jest.runAllTimers();
   });
-  expect(true).toBeTruthy();
+
+  // now the tree's as expected
+  expect(tree).toMatchInlineSnapshot(`
+    <div>
+      true
+    </div>
+  `);
 });
